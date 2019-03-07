4cefa985551f82825d41067460916541640650c6
Arjen van Bochoven <nejra@me.com>
2019-03-08 00:45:33 +0100

    Update CarController.cs

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using DeathRace.Models;
 
 namespace DeathRace.Controllers
@@ -17,45 +18,70 @@ namespace DeathRace.Controllers
         {
           _context = context;
 
-          // if (_context.TodoItems.Count() == 0)
-          // {
-          //     // Create a new TodoItem if collection is empty,
-          //     // which means you can't delete all TodoItems.
-          //     _context.TodoItems.Add(new TodoItem { Name = "Item1" });
-          //     _context.SaveChanges();
-          // }
         }
 
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<ActionResult<IEnumerable<Car>>> GetCars()
         {
-            return new string[] { "value1", "value2" };
+            return await _context.Cars.Include(i => i.User).ToListAsync();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<ActionResult<Car>> GetCar(int id)
         {
-            return "value";
+            var Car = await _context.Cars.Include(i => i.User)
+                .FirstOrDefaultAsync(i => i.CarId == id);
+
+            if (Car == null)
+            {
+                return NotFound();
+            }
+
+            return Car;
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Car>> PostCar(Car user)
         {
+            _context.Cars.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCar), new { CarId = user.CarId }, user);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> PutCar(int id, Car user)
         {
+            if (id != user.CarId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> DeleteCar(int id)
         {
+            var Car = await _context.Cars.FindAsync(id);
+
+            if (Car == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cars.Remove(Car);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
