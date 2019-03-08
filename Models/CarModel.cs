c096d0545cb013d00dbb8d91085d2a3a2cf79773
Arjen van Bochoven <abn290@vu.nl>
2019-03-08 13:10:21 +0100

    Update CarModel.cs
    Add requirements

--- Models/CarModel.cs
+++ Models/CarModel.cs
@@ -7,8 +7,11 @@ namespace DeathRace.Models
     public class Car
     {
         public int CarId { get; set; }
+        [Required(ErrorMessage = "Required")]
         public string Brand { get; set; }
+        [Required(ErrorMessage = "Required")]
         public string Model { get; set; }
+        [Required(ErrorMessage = "Required")]
         public string Type { get; set; }
         [Range(1900, 3000)]
         public int Year { get; set; }
