3ede0dcf284a5c5904b2a508baa649eabf72269f
Arjen van Bochoven <nejra@me.com>
2019-03-12 23:58:34 +0100

    Create DriverDto.cs

---
+++ DeathRace/Dtos/DriverDto.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeathRace.Models
+{
+    public class DriverDto
+    {
+        public int DriverId { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public string GivenName { get; set; }
+        public string Preposition { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public string LastName { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Birth Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public System.DateTime DOB { get; set; }
+
+    }
+}
