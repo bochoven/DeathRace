de0eb9fc4bd3829c29b5e1eefe3311dca55cb501
Arjen van Bochoven <nejra@me.com>
2019-03-12 07:42:24 +0100

    Update site.js
    Push deferred

--- DeathRace/wwwroot/site.js
+++ DeathRace/wwwroot/site.js
@@ -16,35 +16,53 @@ function updateOwners(year = null) {
     var url = "api/Car";
   }
   
-  $.get( url, function( data ) {
-    $('ul.driverlist').empty();
-    var driverList = {};
-    $.each( data, function( index, car ){
-      carList = car.driver.cars;
-      carList.push({
-        carId: car.carId,
-        brand: car.brand,
-        model: car.model,
-        type: car.type,
-        year: car.year
-      })
-      car.driver.cars = carList;
-      car.driver.preposition = car.driver.preposition ? car.driver.preposition : '';
-      driverList[car.driver.driverId] = car.driver;
-    });
-
-    $.each( driverList, function( id, driver ){
-      var cars = $('<ul>');
-      $.each( driver.cars, function( id, car ){
-          cars.append(
-            $('<li>').text(car.brand + ', ' + car.model + ', ' + car.type + ', ' + car.year)
-          )
+  // Retrieve Driver list and filtered car list
+  $.when( $.ajax( "api/Driver" ), $.ajax( url ) ).done(function( a1, a2 ) {
+      allDriverList = a1[ 0 ];
+      filteredCarList = a2[ 0 ];
+      var filteredDriverList = [];
+      var driverIdList = [];
+      
+      // Get driverIdList from filteredCarList
+      $.each( filteredCarList, function( index, car ){
+        if(driverIdList.indexOf(car.driver.driverId) === -1) {
+          driverIdList.push(car.driver.driverId);
+        }
       });
-      $('ul.driverlist').append(
-        $('<li>').text(driver.givenName + ' ' + driver.preposition + ' ' + driver.lastName)
-          .append( cars)
-      );
-    });
+      
+      $.each( allDriverList, function( index, driver ){
+        console.log(driver)
+      });
+  }
+  
+  $.get( "api/Driver", function( data ) {
+    
+    
+    
+    allDriverList = data;
+    
+    $.get( url, function( data ) {
+      $('ul.driverlist').empty();
+      var driverList = {};
+      $.each( data, function( index, car ){
+        car.driver.preposition = car.driver.preposition ? car.driver.preposition : '';
+        driverList[car.driver.driverId] = car.driver;
+      });
+
+      $.each( driverList, function( id, driver ){
+        var cars = $('<ul>');
+        $.each( driver.cars, function( id, car ){
+            cars.append(
+              $('<li>').text(car.brand + ', ' + car.model + ', ' + car.type + ', ' + car.year)
+            )
+        });
+        $('ul.driverlist').append(
+          $('<li>').text(driver.givenName + ' ' + driver.preposition + ' ' + driver.lastName)
+            .append( cars)
+        );
+      });
+
+  });
 
 
   });
