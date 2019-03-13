6b4dc2fcf197fe08d893a37657e36d70a2a3d0f2
Arjen van Bochoven <nejra@me.com>
2019-03-14 00:43:08 +0100

    Update CarDto.cs
    Clean up

--- DeathRace/Dtos/CarDto.cs
+++ DeathRace/Dtos/CarDto.cs
@@ -1,7 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DeathRace.Models
 {
